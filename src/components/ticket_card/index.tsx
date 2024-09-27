0419e964af15d98099019e5665642949591621d6
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-09-27 20:05:38 -0300

    [fix]: warnings on TicketCard

--- src/components/ticket_card/index.tsx
+++ src/components/ticket_card/index.tsx
@@ -63,7 +63,7 @@ function TicketCard({closeModal, ticket}: {closeModal: any, ticket: Ticket}){
 
                     <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal" onClick={closeModal}>
                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
                         <span className="sr-only">Close modal</span>
                     </button>
@@ -91,7 +91,7 @@ function TicketCard({closeModal, ticket}: {closeModal: any, ticket: Ticket}){
                         commentsOnTicket.toReversed().map((comment, index) => {
                             console.log(comment)
                             return (
-                                <div className="bg-gray-200 mt-4 p-2.5 w-full rounded-lg border">
+                                <div className="bg-gray-200 mt-4 p-2.5 w-full rounded-lg border" key={index}>
                                     <div className="flex gap-2">
                                         <div className="bg-black w-8 h-8 rounded-lg text-white text-center content-center font-mono">{comment.User.name[0].toUpperCase()}</div>
                                         <span className="text-md">{comment.User.name}</span>
