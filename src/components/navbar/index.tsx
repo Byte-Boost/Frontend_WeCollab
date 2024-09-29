7a5bfdf8e41d3be8db20b4ede2dab629eb8eaf97
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-09-29 19:24:39 -0300

    [feat]: added way to get to the register page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,25 +1,28 @@
 import './navbar.css'
 import LogOutIcon from "../icons/logout";
-import SettingsIcon from "../icons/settings";
-import AreaTicketIcon from "../icons/area_ticket";
 import TicketIcon from "../icons/ticket";
 import HomeIcon from "../icons/home";
 import Link from 'next/link';
+import AddUserIcon from '../icons/add_user';
+import { useEffect, useState } from 'react';
+import { getSessionUser } from '@/scripts/utils/userService';
 function Navbar(){
+
     return (
         <nav className="navbar navbar--fixed-top max-sm:grid-cols-1 max-sm:grid  ">
             <div className="navbar__inner">
                 <div className="navbar__items navbar__items--left max-sm:justify-center">
-                  
+                <Link href="/home" className="hover:scale-125 transition-all"><HomeIcon/></Link>
                 </div>
                 <div className=" navbar__items navbar__items--right">
                     <div className="flex flex-row gap-6">
-                        <Link href="/home" className="hover:scale-125 transition-all"><HomeIcon/></Link>
+                       
                         <Link href="/ticket" className="hover:scale-125 transition-all"><TicketIcon/></Link>
-                        <Link href="/areatickets" className="hover:scale-125 transition-all"><AreaTicketIcon/></Link>
+                        {/* <Link href="/areatickets" className="hover:scale-125 transition-all"><AreaTicketIcon/></Link> */}
                         {/*
                         <Link href="/settings" className="hover:scale-125 transition-all" ><SettingsIcon/></Link>
                         */}
+                        <Link href="/register" className='hover:scale-125 transition-all'><AddUserIcon/></Link> 
                         <Link href="/" className="hover:scale-125 transition-all"><LogOutIcon/></Link>
                     </div>
                 </div>  
